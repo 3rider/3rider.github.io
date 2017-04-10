398d02e723585c749d8013b01bcf8e22cf858d9d
god <god@3rider.com>
2017-04-10 22:07:05 +0800

    Site updated: 2017-04-10 22:07:05

---
+++ js/cse.js
@@ -0,0 +1,9 @@
+(function() {
+  var cx = '008022951480142452213:87bnucq4uh8';
+  var gcse = document.createElement('script');
+  gcse.type = 'text/javascript';
+  gcse.async = true;
+  gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(gcse, s);
+})();
--- js/script.js
+++ js/script.js
@@ -1,30 +1,22 @@
 (function() {
   $(document).ready(function() {
-    var hideBlocks, offset, showBlocks, timelineBlocks;
     hljs.initHighlightingOnLoad();
-    timelineBlocks = $('.cd-timeline-block');
-    offset = 0.8;
-    hideBlocks = function(blocks, offset) {
-      blocks.each(function() {
-        console.log($(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden'));
-        $(this).offset().top > $(window).scrollTop() + $(window).height() * offset && $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
-      });
-    };
-    showBlocks = function(blocks, offset) {
-      blocks.each(function() {
-        $(this).offset().top <= $(window).scrollTop() + $(window).height() * offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
-      });
-    };
-    hideBlocks(timelineBlocks, offset);
-    $(window).on('scroll', function() {
-      if (!window.requestAnimationFrame) {
-        setTimeout((function() {
-          showBlocks(timelineBlocks, offset);
-        }), 100);
+    setTimeout(function() {
+      return $('input.gsc-input').attr('placeholder', '搜尋文章／內容／標籤？');
+    }, 1000);
+    return window.addEventListener('scroll', function(e) {
+      var distanceY, header, shrinkOn;
+      distanceY = window.pageYOffset || document.documentElement.scrollTop;
+      shrinkOn = 200;
+      header = document.querySelector('#mobile-site-header');
+      if (distanceY > shrinkOn) {
+        if (!classie.has(header, 'show')) {
+          return classie.add(header, 'show');
+        }
       } else {
-        window.requestAnimationFrame((function() {
-          showBlocks(timelineBlocks, offset);
-        }));
+        if (classie.has(header, 'show')) {
+          return classie.remove(header, 'show');
+        }
       }
     });
   });
